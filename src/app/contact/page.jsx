d212c2a0d9508b7661a6083410c046c0b8580761
leofuna <leo.funa@gmail.com>
2023-05-19 19:18:06 -0300

    feat: contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,19 @@
+import { contactDetails } from "@/utils/details";
+
 export default function Contact() {
+  const { email, phone } = contactDetails;
   return (
-    <main>
-      My Contact
+    <main className="container mx-auto max-width section">
+      <h1 className="text-center text-2xl md:text-3xl lg:text-6xl text-dark-heading dark:text-light-heading font-semibold md:font-bold">
+        Fique a vontade para entrar em contato
+      </h1>
+      <h3 className="text-center text-3xl md:text-4xl lg:text-6xl text-gradient font-semibold md:font-bold pt-5 md:pt-10 md:pb-6">
+        <a href={`mailto:${email}`}>{email}</a>
+      </h3>
+      <span className="text-center text-content text-xl font-light block">or</span>
+      <h3 className="text-center text-3xl md:text-4xl lg:text-6xl text-gradient font-semibold md:font-bold pt-2 md:py-6">
+        <a href={`tel:${phone}`}>{phone}</a>
+      </h3>
     </main>
-  )
+  );
 }
--- src/utils/details.js
+++ src/utils/details.js
@@ -43,3 +43,51 @@ export const eduDetails = [
     Duration: "Jul 2010 - Jul 2017",
   },
 ];
+
+export const projectDetails = [
+  {
+    title: "Account Manager",
+    imagePath: '/projects/generalbg.jpg',
+    description: `Nesse projeto desenvolvi uma api que visa simular interações de contas em um banco. Alguns exemplo: transferir, depositar, saldo`,
+    techstack: "Express, Node Js, Jest, MongoDb, JWT",
+    previewLink: "https://github.com/LeoFuna/account-manager",
+    githubLink: "https://github.com/LeoFuna/account-manager",
+  },
+  {
+    title: "Quem sou eu?",
+    imagePath: '/projects/whoami.png',
+    description: `Aplicação voltada a gestão de login de usuários em rotas públicas e privadas`,
+    techstack: "React, Hooks, Next Js, MySQL, Tailwind CSS, Typescript, Prisma, Node Js, oAuth, Cookies",
+    previewLink: "https://who-am-i-ten.vercel.app/",
+    githubLink: "https://github.com/LeoFuna/who-am-i",
+  },
+  {
+    title: "Gestor de Milhas",
+    imagePath: '/projects/handlemiles.png',
+    description: `Sistema básico para gestão de milhas e pontos das companhias aéreas`,
+    techstack: "React, Next Js, Firebase, noSQL Database, Typescript, Cache, Material UI, React Hook Form",
+    previewLink: "https://handle-miles.vercel.app/auth/signin",
+    githubLink: "https://github.com/LeoFuna/handle-miles",
+  },
+  {
+    title: "Netflix Clone",
+    imagePath: '/projects/netflixclone.png',
+    description: `Projeto feito no inícios dos meus estudos de programação, onde busquei clonar o principal do site da Nextlix`,
+    techstack: "React, Styled Components, Github Pages",
+    previewLink: "https://leofuna.github.io/Netflix-Clone/",
+    githubLink: "https://github.com/LeoFuna/Netflix-Clone",
+  },
+  {
+    title: "Projeto Trivia",
+    imagePath: '/projects/triviaproject.png',
+    description: `Um jogo de perguntas e respostas onde você ganha pontuação pelas respostas corretas.`,
+    techstack: "React, Redux, Javascript, Html, Css",
+    previewLink: "https://leofuna.github.io/trivia-project/",
+    githubLink: "https://github.com/LeoFuna/trivia-project",
+  },
+];
+
+export const contactDetails = {
+  email: "leo.funa@gmail.com",
+  phone: "+55 27 99829 5835",
+};
