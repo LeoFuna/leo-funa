5fa513c7df92a0de51a003b440e1b724e9fbc224
leofuna <leo.funa@gmail.com>
2023-05-19 19:17:53 -0300

    feat: projects page

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,7 +1,31 @@
+import React from "react";
+import { projectDetails } from "@/utils/details";
+import Project from "@/components/Project";
+
 export default function Projects() {
   return (
-    <main>
-      My Projects
+    <main className="container mx-auto max-width pt-10 mb-20">
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Projects
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10">
+          {React.Children.toArray(
+            projectDetails.map(
+              ({ title, imagePath, description, techstack, previewLink, githubLink }) => (
+                <Project
+                  title={title}
+                  imagePath={imagePath}
+                  description={description}
+                  techstack={techstack}
+                  previewLink={previewLink}
+                  githubLink={githubLink}
+                />
+              )
+            )
+          )}
+        </div>
+      </section>
     </main>
-  )
+  );
 }
