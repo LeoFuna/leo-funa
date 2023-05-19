64bff695c283b9b361059d81be33b49ff7bffc19
leofuna <leo.funa@gmail.com>
2023-05-19 12:08:44 -0300

    fix: removing custom next font from layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,6 @@
 import Header from '@/components/Header'
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Leonardo Funabashi',
@@ -13,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>
+      <body>
         <Header />
         {children}
         <Footer />
