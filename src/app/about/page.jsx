37be73289ca775580dcf8f9fa2c5038563b871f1
leofuna <leo.funa@gmail.com>
2023-05-19 17:12:57 -0300

    feat: about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,48 @@
+import React from "react";
+import Work from "@/components/Work";
+import { personalDetails, workDetails, eduDetails } from "@/utils/details";
+
 export default function About() {
   return (
-    <main>
-      My About
+    <main className="container mx-auto max-width pt-10 pb-20 ">
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Sobre mim
+        </h1>
+        <p className="text-content py-8 lg:max-w-3xl">{personalDetails.about}</p>
+      </section>
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Experiências
+        </h1>
+        {React.Children.toArray(
+          workDetails.map(({ Position, Company, Location, Type, Duration }) => (
+            <Work
+              position={Position}
+              company={Company}
+              location={Location}
+              type={Type}
+              duration={Duration}
+            />
+          ))
+        )}
+      </section>
+      <section>
+        <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Educação
+        </h1>
+        {React.Children.toArray(
+          eduDetails.map(({ Position, Company, Location, Type, Duration }) => (
+            <Work
+              position={Position}
+              company={Company}
+              location={Location}
+              type={Type}
+              duration={Duration}
+            />
+          ))
+        )}
+      </section>
     </main>
-  )
+  );
 }
--- src/utils/details.js
+++ src/utils/details.js
@@ -1,7 +1,7 @@
 export const personalDetails = {
   name: "Leonardo Funabashi",
   tagline: "Eu desenvolvo soluções para web",
-  about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+  about: `Sou um amante de tecnologia, finanças, inovação e música que começou a trabalhar como desenvolvedor para trazer ideias à realidade. Fato curioso eu sei tocar 4 instrumentos musicais.`,
 };
 
 export const socialMediaUrl = {
@@ -9,3 +9,37 @@ export const socialMediaUrl = {
   github: "https://www.github.com/LeoFuna",
   instagram: "https://www.instagram.com/",
 };
+
+export const workDetails = [
+  {
+    Position: "Desenvolvedor Full Stack Pleno",
+    Company: "Digital Republic",
+    Location: "SP - Brasil",
+    Type: "Remoto",
+    Duration: "Jan 2023 - Atual",
+  },
+  {
+    Position: "Desenvolvedor Full Stack",
+    Company: "Digital Republic",
+    Location: "SP - Brasil",
+    Type: "Remoto",
+    Duration: "Dez 2021 - Dez 2022",
+  },
+];
+
+export const eduDetails = [
+  {
+    Position: "Desenvolvimento Web",
+    Company: `Trybe`,
+    Location: "MG",
+    Type: "Remoto",
+    Duration: "Mar 2021 - Mar 2022",
+  },
+  {
+    Position: "Bacharel em Engenharia de Produção",
+    Company: "Instituto Federal do Espírito Santo",
+    Location: "ES",
+    Type: "Presencial",
+    Duration: "Jul 2010 - Jul 2017",
+  },
+];
